74c823fc2b79359aea602a660cd7133b65033e38
Steven Solomon <solomon.steven.m@gmail.com>
2020-06-24 14:09:46 -0400

    Adds strong params

--- app/controllers/option_trades_controller.rb
+++ app/controllers/option_trades_controller.rb
@@ -1,6 +1,6 @@
 class OptionTradesController < ApplicationController
   def create
-    option_trade = OptionTrade.create(action: 'call', strike: 10.0, premium: 1.0)
+    option_trade = OptionTrade.create(option_trade_params)
     render json: option_trade.present
   end
 
@@ -9,4 +9,8 @@ class OptionTradesController < ApplicationController
   end
 
   private
+
+  def option_trade_params
+    params.require(:option_trade).permit(:action, :strike, :premium)
+  end
 end
--- spec/request/option_trades_spec.rb
+++ spec/request/option_trades_spec.rb
@@ -1,13 +1,23 @@
 require 'rails_helper'
 
 describe 'OptionTrades', type: :request do
-  it 'creates a trade' do
-    post '/option_trades', headers: {'ACCEPT' => 'application/json'}, params: {action: 'call', strike: 10.0, premium: 1.0}
+  it 'creates a call option trade' do
+    post '/option_trades', headers: {'ACCEPT' => 'application/json'}, params: {option_trade: {action: 'call', strike: 10.0, premium: 1.0}}
 
     expect(JSON.parse(response.body)).to eq({"action" => 'call', "strike" => 10.0, "premium" => 1.0})
 
     get '/option_trades', headers: {'Content-Type' => 'application/json'}
 
     expect(JSON.parse(response.body)).to eq([{"action" => 'call', "strike" => 10.0, "premium" => 1.0}])
+  end
+
+  it 'creates a put option trade' do
+    post '/option_trades', headers: {'ACCEPT' => 'application/json'}, params: {option_trade: {action: 'put', strike: 10.0, premium: 1.0}}
+
+    expect(JSON.parse(response.body)).to eq({"action" => 'put', "strike" => 10.0, "premium" => 1.0})
+
+    get '/option_trades', headers: {'Content-Type' => 'application/json'}
+
+    expect(JSON.parse(response.body)).to eq([{"action" => 'put', "strike" => 10.0, "premium" => 1.0}])
   end
 end
