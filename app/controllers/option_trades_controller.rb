f870bbe3d898aa5bee48e710760c78dc8098a999
Steven Solomon <solomon.steven.m@gmail.com>
2020-06-25 12:25:03 -0400

    Correct endpoint to not authenticate CSRF

--- app/controllers/option_trades_controller.rb
+++ app/controllers/option_trades_controller.rb
@@ -1,4 +1,6 @@
 class OptionTradesController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def create
     option_trade = OptionTrade.create(option_trade_params)
     render json: option_trade.present
--- spec/request/option_trades_spec.rb
+++ spec/request/option_trades_spec.rb
@@ -1,23 +1,32 @@
 require 'rails_helper'
 
 describe 'OptionTrades', type: :request do
-  it 'creates a call option trade' do
-    post '/option_trades', headers: {'ACCEPT' => 'application/json'}, params: {option_trade: {action: 'call', strike: 10.0, premium: 1.0}}
+  let (:params) do
+    {option_trade: {action: action, strike: 10.0, premium: 1.0}}
+  end
+  describe 'when calling' do
+    let(:action) { 'call' }
+    it 'creates a call option trade' do
+      post '/option_trades', headers: {'ACCEPT' => 'application/json'}, params: params
 
-    expect(JSON.parse(response.body)).to eq({"action" => 'call', "strike" => 10.0, "premium" => 1.0})
+      expect(JSON.parse(response.body)).to eq({"action" => 'call', "strike" => 10.0, "premium" => 1.0})
 
-    get '/option_trades', headers: {'Content-Type' => 'application/json'}
+      get '/option_trades', headers: {'Content-Type' => 'application/json'}
 
-    expect(JSON.parse(response.body)).to eq([{"action" => 'call', "strike" => 10.0, "premium" => 1.0}])
+      expect(JSON.parse(response.body)).to eq([{"action" => 'call', "strike" => 10.0, "premium" => 1.0}])
+    end
   end
 
-  it 'creates a put option trade' do
-    post '/option_trades', headers: {'ACCEPT' => 'application/json'}, params: {option_trade: {action: 'put', strike: 10.0, premium: 1.0}}
+  describe 'when putting' do
+    let(:action) { 'put' }
+    it 'creates a put option trade' do
+      post '/option_trades', headers: {'ACCEPT' => 'application/json'}, params: params
 
-    expect(JSON.parse(response.body)).to eq({"action" => 'put', "strike" => 10.0, "premium" => 1.0})
+      expect(JSON.parse(response.body)).to eq({"action" => 'put', "strike" => 10.0, "premium" => 1.0})
 
-    get '/option_trades', headers: {'Content-Type' => 'application/json'}
+      get '/option_trades', headers: {'Content-Type' => 'application/json'}
 
-    expect(JSON.parse(response.body)).to eq([{"action" => 'put', "strike" => 10.0, "premium" => 1.0}])
+      expect(JSON.parse(response.body)).to eq([{"action" => 'put', "strike" => 10.0, "premium" => 1.0}])
+    end
   end
 end
